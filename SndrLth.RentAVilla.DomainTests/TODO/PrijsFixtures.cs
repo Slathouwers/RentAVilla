b0b1927b971e3bf6fb10224e56f26b6ad9258462
Slathouwers <sander.lathouwers@live.be>
2019-04-19 23:56:48 +0200

    Created Testfixtures for full coverage of future implementation

---
+++ SndrLth.RentAVilla.DomainTests/TODO/PrijsFixtures.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SndrLth.RentAVilla.DomainTests
+{
+    [TestClass]
+    public class PrijsFixtures
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SndrLth.RentAVilla.DomainTests/TODO/StaffelkortingFixtures.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SndrLth.RentAVilla.DomainTests
+{
+    [TestClass]
+    public class StaffelkortingFixtures
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
